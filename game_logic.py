0bc59220672199e9afdc7bed691194c783672beb
Amon Soares de Souza <amonsoares@Amons-MBP.fritz.box>
2019-03-22 12:31:12 +0100

    update game_logic

--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,25 @@
 import random
-from . import word_processing
+import word_processing
 
+words = word_processing.get_words()
+
+
+def start_game():
+    print("Welcome to Hangman!")
+    choice_1 = input("Would you like to play? y/n")
+    if choice_1 == "y":
+        game()
+    else:
+        print("Alrighty then, ye butthole.")
+
+
+def game():
+    random_word = words[random.randint(0, len(words))]
+    enumerated_word = word_processing.enum_word(random_word)
+    guesses = 8
+    print("Alright, let's start! You have 8 guesses to find the missing word!")
+    
+    while guesses != 0:
+        guess = input("What character do you want to pick?")
+        if guess in enumerated_word.values()
 
--- word_processing.py
+++ word_processing.py
@@ -2,28 +2,20 @@ import nltk
 from nltk.corpus import gutenberg
 
 
-class TextProcessing:
-
-    def __init__(self):
-        pass
+def check_pos(lst):
+    return [word for word,pos in nltk.pos_tag(lst) if pos.startswith("N")]
 
 
-    @staticmethod
-    def check_pos(lst):
-        return [word for word,pos in nltk.pos_tag(lst) if pos.startswith("N")]
+def get_words():
+    bag = []
+    for text in gutenberg.fileids():
+        if len(bag) > 10000:
+            break
+        for word in gutenberg.words(text):
+            if len(word) > 8:
+                bag.append(word)
+    return check_pos(bag)
 
 
-    @staticmethod
-    def get_words():
-        bag = []
-        for text in gutenberg.fileids():
-            if len(bag) > 10000:
-                break
-            for word in gutenberg.words(text):
-                if len(word) > 7:
-                    bag.append(word)
-        return check_pos(bag)
-
-    @staticmethod
-    def enum_word(word):
-        return {num: char for num, char in enumerate(word)}
+def enum_word(word):
+    return {num: char for num, char in enumerate(word)}
